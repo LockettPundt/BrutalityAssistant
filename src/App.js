485f2e3a13e65a90b66e27f92cf133b98a281e6f
Lockett Pundt <kalifornium@hotmail.com>
2020-05-15 10:36:00 -0400

    imported react router as well as the user and job components. I'm not 100% sure i'll make this more than a single page app but i have the routes in place if so.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router, Route, Switch, Link,
+} from 'react-router-dom';
+import { Box } from 'grommet';
+import UserRegister from './components/UserRegister';
+import NewJob from './components/NewJob';
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          hi there this is the client side.
+        </Route>
+        <Route path="/userregister" component={UserRegister} />
+        <Route path="/newjob" component={NewJob} />
+      </Switch>
+    </Router>
   );
 }
 
