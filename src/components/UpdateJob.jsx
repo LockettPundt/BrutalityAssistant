0a78a89798385c2740e6ad30a4d269ba1ec1a4c2
Lockett Pundt <kalifornium@hotmail.com>
2020-05-20 12:39:09 -0400

    component to update or delete a user's job post.

---
+++ src/components/UpdateJob.jsx
@@ -0,0 +1,110 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import {
+  Form, FormField, Box, Button, TextInput, Select,
+} from 'grommet';
+import axios from 'axios';
+import API_URL from '../utils/appUtils';
+
+const UpdateJob = () => {
+  const { id } = useParams();
+  const [company, setCompany] = useState('');
+  const [position, setPosition] = useState('');
+  const [skillsNeeded, setSkillsNeeded] = useState('');
+  const [interview, setInterview] = useState('Interviewed?');
+  const [userEmail, setUserEmail] = useState(localStorage.getItem('userEmail') || null);
+  const history = useHistory();
+
+  const updateJob = async () => {
+    const response = await axios.put(`${API_URL}jobs/${id}`, {
+      company,
+      position,
+      skillsNeeded,
+      interview: interview === 'True',
+    });
+    history.push('/myjobs');
+  };
+
+  const removeJob = async () => {
+    const response = await axios.delete(`${API_URL}jobs/${id}`);
+    history.push('/myjobs');
+  };
+
+
+  useEffect(() => {
+    const jobInfo = async (jobId) => {
+      const response = await axios.get(`${API_URL}jobs/${jobId}`);
+      // console.log(response);
+      setCompany(response.data[0].company);
+      setInterview(response.data[0].interview ? 'True' : 'False');
+      setSkillsNeeded(response.data[0].skillsNeeded.join(', '));
+      setPosition(response.data[0].position);
+    };
+
+    jobInfo(id);
+  }, []);
+
+  return (
+    <Box
+      direction="column"
+      margin={{
+        horizontal: 'auto',
+        top: 'xlarge',
+      }}
+      responsive
+      width="medium"
+    >
+      <Form onSubmit={updateJob}>
+        <FormField>
+          <TextInput
+            value={company}
+            required
+            onChange={(e) => setCompany(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <TextInput
+            value={position}
+            required
+            onChange={(e) => setPosition(e.target.value)}
+          />
+        </FormField>
+
+        <FormField>
+          <TextInput
+            value={skillsNeeded}
+            required
+            onChange={(e) => setSkillsNeeded(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <Select
+            placeholder={interview}
+            options={['True', 'False']}
+            onChange={({ option }) => setInterview(option)}
+          />
+        </FormField>
+        <Box
+          direction="row"
+
+        >
+          <Button
+            type="submit"
+            label="Update"
+          />
+          <Button
+            type="button"
+            label="Remove"
+            onClick={removeJob}
+          />
+
+        </Box>
+
+      </Form>
+    </Box>
+  );
+};
+
+
+export default UpdateJob;
