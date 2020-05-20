8c7b006af781e818f44ee9d97282f0e3e7c452fb
Lockett Pundt <kalifornium@hotmail.com>
2020-05-20 12:39:49 -0400

    deleted console log in there.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
-  BrowserRouter as Router, Route, Switch,
+  BrowserRouter as Router, Route,
 } from 'react-router-dom';
 import { Grommet } from 'grommet';
 import UserRegister from './components/UserRegister';
@@ -9,21 +9,24 @@ import NewJob from './components/NewJob';
 import LogIn from './components/LogIn';
 import Index from './components/Index';
 import MyJobs from './components/MyJobs';
+import UpdateJob from './components/UpdateJob';
+import Navbar from './components/Nav';
 
 function App() {
   const [userEmail, setUserEmail] = useState(localStorage.getItem('userEmail') || null);
   const [userToken, setUserToken] = useState(localStorage.getItem('token') || null);
-  console.log(localStorage.getItem('userEmail'), localStorage.getItem('token'));
+
   return (
     <Grommet>
       <Router>
-        <Switch>
-          <Route exact path="/" component={() => <Index userEmail={userEmail} userToken={userToken} />} />
-          <Route path="/register" component={() => <UserRegister setUserEmail={setUserEmail} setUserToken={setUserToken} />} />
-          <Route path="/myjobs" component={() => <MyJobs />} />
-          <Route path="/newjob" component={NewJob} />
-          <Route path="/login" component={() => <LogIn setUserEmail={setUserEmail} setUserToken={setUserToken} />} />
-        </Switch>
+        <Navbar />
+        <Route exact path="/" component={() => <Index userEmail={userEmail} userToken={userToken} />} />
+        <Route path="/register" component={() => <UserRegister setUserEmail={setUserEmail} setUserToken={setUserToken} />} />
+        <Route path="/myjobs" component={() => <MyJobs />} />
+        <Route path="/updatejob/:id" component={() => <UpdateJob />} />
+        <Route path="/newjob" component={NewJob} />
+        <Route path="/login" component={() => <LogIn setUserEmail={setUserEmail} setUserToken={setUserToken} />} />
+
       </Router>
     </Grommet>
   );
