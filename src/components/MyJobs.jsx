f53624b81fff905ddd07a0326dffeca2cb9d2b34
Lockett Pundt <kalifornium@hotmail.com>
2020-05-21 16:33:58 -0400

    created a seperate component to render for smaller screens as the data table can't shrink past a certain width. ( or i can't figure it out at the moment.). created anotehr useEffect to get the screen size on resize and adjust what is rendered when it passes a width of 650.

--- src/components/MyJobs.jsx
+++ src/components/MyJobs.jsx
@@ -13,6 +13,13 @@ const MyJobs = () => {
   const [jobs, setJobs] = useState([]);
   const [userEmail, setUserEmail] = useState(localStorage.getItem('userEmail') || null);
   const history = useHistory();
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  const resizeWindow = () => {
+    setWindowHeight(window.innerHeight);
+    setWindowWidth(window.innerWidth);
+  };
 
   useEffect(() => {
     const getJobs = async (email) => {
@@ -30,62 +37,115 @@ const MyJobs = () => {
     };
     const auth = async () => {
       const response = await userAuth();
-      console.log('response in my jobs', response);
+      // console.log('response in my jobs', response);
       if (response) getJobs(response.data.authorizedData.UserInfo.email);
       else history.push('/');
     };
     auth();
   }, [history]);
 
+  useEffect(() => {
+    // console.log(windowHeight, windowWidth);
+    resizeWindow();
+    window.addEventListener('resize', resizeWindow);
+    return () => window.removeEventListener('resize', resizeWindow);
+  });
+
+  const jobCards = jobs.map((job) => (
+    <Box
+      background="pink"
+      pad="medium"
+      round="xsmall"
+      margin={{
+        vertical: 'small',
+        horizontal: 'small',
+      }}
+    >
+      <Text>
+        Company:
+        {' '}
+        {job.company}
+      </Text>
+      <Text>
+        Position:
+        {' '}
+        {job.position}
+      </Text>
+      <Text>
+        Date:
+        {' '}
+        {job.date}
+      </Text>
+      <Text>
+        Skills Needed:
+        {' '}
+        {job.skillsNeeded}
+      </Text>
+      <Text>
+        Interview Status:
+        {' '}
+        {job.interview ? 'Yes' : 'No'}
+      </Text>
+      <Button
+        color="rgb(29, 29, 29)"
+        alignSelf="center"
+        type="button"
+        label="Update"
+        href={job.update}
+      />
+    </Box>
+  ));
 
   return (
     <Box
       margin={{
         horizontal: 'auto',
-        top: 'large',
+        top: 'small',
       }}
       alignSelf="center"
       responsive
-      width="large"
     >
-      <DataTable
-        responseive
-        columns={[
-          {
-            property: 'company',
-            header: <Text>Company</Text>,
-            primary: true,
-          },
-          {
-            property: 'position',
-            header: <Text>Position</Text>,
-          },
-          {
-            property: 'date',
-            header: <Text>Date Applied</Text>,
-          },
-          {
-            property: 'skillsNeeded',
-            header: <Text>Skills Needed</Text>,
-          },
-          {
-            property: 'interview',
-            header: <Text>Interview</Text>,
-          },
-          {
-            property: 'update',
-            render: (update) => (
-              <Button
-                type="button"
-                label="Update"
-                href={update.update}
-
-              />
-            ),
-          },
-        ]}
-        data={jobs}
-      />
+      {windowWidth >= 650
+        ? (
+          <DataTable
+            responsive
+            columns={[
+              {
+                property: 'company',
+                header: <Text>Company</Text>,
+                primary: true,
+              },
+              {
+                property: 'position',
+                header: <Text>Position</Text>,
+              },
+              {
+                property: 'date',
+                header: <Text>Date Applied</Text>,
+              },
+              {
+                property: 'skillsNeeded',
+                header: <Text>Skills Needed</Text>,
+              },
+              {
+                property: 'interview',
+                header: <Text>Interview</Text>,
+              },
+              {
+                property: 'update',
+                render: (update) => (
+                  <Button
+                    type="button"
+                    label="Update"
+                    href={update.update}
+                  />
+                ),
+              },
+            ]}
+            data={jobs}
+          />
+        )
+        : jobCards}
       <Button
         type="button"
         href="/newjob"
